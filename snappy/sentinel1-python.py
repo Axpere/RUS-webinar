38f4a3ffb5cf1f29a3c940ef4b4e1473963cd45b
Axpere <11291858+Axpere@users.noreply.github.com>
2021-02-03 11:39:07 +0100

    Added Preprocessing part.
    missing df_s1_read dictionary data frame part from the video

--- snappy/sentinel1-python.py
+++ snappy/sentinel1-python.py
@@ -44,3 +44,36 @@ def output_view(product, band, min_value_VV, max_value_VV, min_value_VH, max_val
     fig, (ax1,ax2) = plt.subplots(1,2, figsize=(16,16))
     ax1.imshow(band_data_list[0], cmap='gray', vmin=min_value_VV , vmax=max_value_VV)
     ax1.set_title(output_bands[0])
+    ax2.imshow(band_data_list[1], cmap='gray', vmin=min_value_VH , vmax=max_value_VH)
+    ax2.set_title(output_bands[1])
+    
+    for ax in fig.get_axes():
+      ax.label_outer()
+      
+#Sentinel-1 Preporocessing: snappy
+
+#Call gpt -h from command line to see operators available in snappy. for specific operator call "gpt -h *operator*"
+print(subprocess.Popen(['gpt', '-h', 'ThermalNoiseRemoval'], stdout=subprocess.PIPE, universal_newlines=True).communicate()[0])
+                       
+#Stepps of pre-processing to generate analysis-ready data:
+#Read->Subset->Apply Orbit File->Thermal Noise Removal->Calibration->Speckle Filtering->Terrain Correction->Write
+
+#Read Product
+#Set target folder and extract metadata
+product_path = "/shared/Training/PY01_SentinelProcessing_snappy/Original" #Change this path with the source folder (source file in .zip)
+input_S1_files = sorted(list(iglob(join(product_path, '**', '*S1*.zip'), recursive=True)))
+
+name, sensing_mode, produt_type, polarization, height, width, band_names = ([] for i in range (7))
+
+for i in input_S1_files:                        #extracting metadata from the product name
+  sensing_mode.append(i.split("_")[3])
+  product_type.append(i.split("_")[4])
+  polarization.append(i.split("_")[-6])
+  #REad with snappy
+  s1_read = snappy.ProductIO.readProduct(i)
+  name.append(s1_read.getName())
+  height.append(s1_read.getSceneRasterHeight())
+  width.append(s1_read.getSceneRasterWidth())
+  band_names.append(S1_read.getBandNames())
+  
+df_s1_read = pd.DataFrame({'Name': name, 'Sensing Mode', 'Product Type': product_type, 'Polarization': polarization, 'Height': height , (xxxx) display(df_s1_read)
